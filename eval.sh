f06d7abc06b8e091d3e00f314cdd6e4c2f382778
蒋硕 <111181768+JSLiam94@users.noreply.github.com>
2024-10-07 19:25:48 +0800

    Update eval.sh
    all modality group eval

--- eval.sh
+++ eval.sh
@@ -1,12 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+# 获取输入参数，即 GPU 设备编号
+gpu_device=$1
+
+# 原始脚本中定义的其他变量
 time=$(date "+%Y%m%d-%H%M%S")
-name=Eval_mode1_BraTS18_ShaSpec_[80,160,160]_SGD_b1_lr-2_alpha.1_beta.02_trainOnly_rand_mode
+name="Eval_mode1_BraTS24_ShaSpecval"
 
-CUDA_VISIBLE_DEVICES=$1 python eval.py \
---input_size=80,160,160 \
---num_classes=3 \
---data_list=BraTS18/BraTS18_test.csv \
---weight_std=True \
---restore_from=snapshots/BraTS18_ShaSpec_[80,160,160]_SGD_b1_lr-2_alpha.1_beta.02_trainOnly_rand_mode/final.pth \
---mode=0,1,2,3 > logs/${time}_train_${name}.log
+# 函数：执行单次评估
+run_evaluation() {
+    local mode="$1"
+    CUDA_VISIBLE_DEVICES=${gpu_device} python eval.py \
+        --input_size=80,160,160 \
+        --num_classes=3 \
+        --data_list=val.csv \
+        --weight_std=True \
+        --restore_from=snapshots/BraTS24_ShaSpecval/best.pth \
+        --mode="${mode}" > logs/${time}_train_${name}_${mode}.log
+}
+
+# 手动列出所有可能的模式组合
+modes="
+0,1,2,3
+0
+1
+2
+3
+0,1
+0,2
+0,3
+1,2
+1,3
+2,3
+0,1,2
+0,1,3
+0,2,3
+1,2,3
+"
+
+# 遍历所有模式组合并执行评估
+for mode in $modes; do
+    run_evaluation "$mode"
+done
