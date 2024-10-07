337b308dc6e9e0639a0a0466bd8d350eb382d197
蒋硕 <111181768+JSLiam94@users.noreply.github.com>
2024-10-07 19:30:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 time=$(date "+%Y%m%d-%H%M%S")
-name=BraTS24_ShaSpec_[80,160,160]_SGD_b1_lr-2_alpha.1_beta.02_trainOnly
+name=BraTS24_ShaSpec
 
 CUDA_VISIBLE_DEVICES=$1 python train_SS.py \
 --snapshot_dir=snapshots/$name/ \
@@ -9,39 +8,15 @@ CUDA_VISIBLE_DEVICES=$1 python train_SS.py \
 --batch_size=1 \
 --num_gpus=1 \
 --num_steps=80000 \
---val_pred_every=2000 \
+--val_pred_every=10000 \
 --learning_rate=1e-2 \
 --num_classes=3 \
 --num_workers=4 \
---train_list=train_all.csv \
+--train_list=train.csv \
 --val_list=val.csv \
 --random_mirror=True \
 --random_scale=True \
 --weight_std=True \
---train_only \
---reload_path=snapshots/tmp/final.pth \
---reload_from_checkpoint=False > logs/${time}_train_${name}.log
-
-
-time=$(date "+%Y%m%d-%H%M%S")
-name=BraTS24_ShaSpec_[80,160,160]_SGD_b1_lr-2_alpha.1_beta.02_trainOnly_rand_mode
-
-CUDA_VISIBLE_DEVICES=$1 python train_SS.py \
---snapshot_dir=snapshots/$name/ \
---input_size=80,160,160 \
---batch_size=1 \
---num_gpus=1 \
---num_steps=180000 \
---val_pred_every=2000 \
---learning_rate=1e-2 \
---num_classes=3 \
---num_workers=4 \
---train_list=train_all.csv \
---val_list=val.csv \
---random_mirror=True \
---random_scale=True \
---weight_std=True \
---train_only \
---reload_path=snapshots/BraTS18_ShaSpec_[80,160,160]_SGD_b1_lr-2_alpha.1_beta.02_trainOnly/final.pth \
+--reload_path=snapshots/BraTS24_ShaSpec1/last.pth \
 --reload_from_checkpoint=True \
---mode=random > logs/${time}_train_${name}.log
+--mode=random #> logs/${time}_train_${name}.log
