614630d96951fcd514c39a8d48aa992425c744a8
蒋硕 <111181768+JSLiam94@users.noreply.github.com>
2024-12-30 23:54:33 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -18,25 +18,22 @@ def generate_csv_from_folders(root_dir, output_csv):
     # 将路径列表转换为DataFrame
     df = pd.DataFrame(paths, columns=['path'])
 
+
     # 将DataFrame保存为CSV文件
     df.to_csv(output_csv, index=False)
 
 # 设置根目录和输出CSV文件名
-root_directory = '/root/autodl-tmp/BraTS2024/train'
-output_csv_file = './output.csv'
+train_data = '/home/zhushenghao/data/ZSH/datasets/FeTS2024/train'
+test_data = '/home/zhushenghao/data/ZSH/datasets/FeTS2024/test'
+train_csv = './train.csv'
+test_csv = './test.csv'
 
 # 生成CSV文件
-generate_csv_from_folders(root_directory, output_csv_file)
+generate_csv_from_folders(train_data, train_csv)
+generate_csv_from_folders(test_data, test_csv)
 
-# 读取CSV文件
-df = pd.read_csv(output_csv_file)
 
-# 划分数据集
-train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
 
-# 保存结果
-train_df.to_csv('train.csv', index=False)
-val_df.to_csv('val.csv', index=False)
 
 print("Train CSV file saved as 'train.csv'")
 print("Validation CSV file saved as 'val.csv'")
